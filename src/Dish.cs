4edcc0e4a3995aa19c63f8d4457ea61e774f31c3
Thomas <thomandoclases@gmail.com>
2025-09-02 11:22:13 -0300

    Class dich creada

--- src/Dish.cs
+++ src/Dish.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices.JavaScript;
+
 namespace Ucu.Poo.Restaurant
 {
 
@@ -6,5 +8,16 @@ namespace Ucu.Poo.Restaurant
     /// </summary>
     public class Dish
     {
+        public string name { get; private set; }
+        public int price  { get; private set; }
+        public bool isVegetarian  { get; private set; }
+
+        public Dish(string name, int price, bool isVegetarian)
+        {
+            this.name = name;
+            this.price = price;
+            this.isVegetarian = isVegetarian;
+        }
+        
     }
 }
